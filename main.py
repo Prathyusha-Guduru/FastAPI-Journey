6a4d5cbc779920cb73a855475f08ed7154d838e5
Prathyusha-Guduru <prathyushaguduru28@gmail.com>
2023-02-19 22:14:47 +0530

    Update : main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from typing import Union
+
 from fastapi import FastAPI
-
-class User(pydantic.BaseModel):
-    username : str
-    password : str
-    age : int
-    score : float
-    
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
 @app.get("/")
 def read_root():
-    return  {"Hello":"World"}
+    return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
